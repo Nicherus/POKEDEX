b7a90ee58c6c5aed9c5851c23dae7d077eb38c66
Nicherus <joaonichev@gmail.com>
2020-10-16 00:25:11 -0300

    chore: getting pokemons object from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [pokemons, setPokemons] = useState({});
+
+  useEffect(() => {
+    const request = axios.get("https://pokeapi.co/api/v2/pokemon?limit=893");
+    request.then(res => {
+      setPokemons(res.data.results);
+    })
+  }, []);
+
+
   return (
     <>
     </>
